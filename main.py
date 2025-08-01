c62c4a875e6261bc8a063cdc18b9ac98dca10f1d
danesm03 <danesmithhh@gmail.com>
2025-07-31 22:12:26 -0400

    added asteroids and asteroid field. no collision detection yet

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import *
 from player import Player
 from circleshape import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -13,10 +15,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = updatable
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
