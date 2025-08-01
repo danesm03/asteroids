9c9be2d3c754b0774b0f7360519c097ab82a9630
danesm03 <danesmithhh@gmail.com>
2025-07-31 21:52:53 -0400

    add rotation and forwards/backwards movement, as well as groups/containers

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
 from circleshape import *
 
 def main():
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,12 +26,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        updatable.update(dt)
+
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-
-
+    
         #limit framerate to 60fps
         dt = clock.tick(60) / 1000
        #print(f"dt:{dt}")
